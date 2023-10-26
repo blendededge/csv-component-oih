fd08d288db45a611fa21bbb3d70dc0cb254fcb64
winklerj <robb@blendededge.com>
2023-10-26 13:17:10 -0400

    Added try/catch to emit events through ferryman-extensions

--- lib/actions/writeStream.ts
+++ lib/actions/writeStream.ts
@@ -5,19 +5,30 @@ import { writeCSV } from './write';
 import { Snapshot, Message, Config, IncomingHeaders, TokenData } from '@blendededge/ferryman-extensions/lib/ferryman-types';
 
 async function writeStream(this: Self, msg: Message, cfg: Config, snapshot: Snapshot, headers: IncomingHeaders, tokenData: TokenData) {
-    const newMsg = formatMessage(msg);
-    const emitter = await wrapper(this, newMsg, cfg, snapshot, headers, tokenData);
-    const token = cfg.token || tokenData.apiKey;
+    let emitter;
+    try {
+        const newMsg = formatMessage(msg);
+        emitter = await wrapper(this, newMsg, cfg, snapshot, headers, tokenData);
+        const token = cfg.token || tokenData.apiKey;
 
-    const { data } = newMsg;
-    if (data.items === null || typeof data.items !== 'object' || Array.isArray(data.items)) {
-        await errorHelper(emitter, 'Input data must be Object');
+        const { data } = newMsg;
+        if (data.items === null || typeof data.items !== 'object' || Array.isArray(data.items)) {
+            await errorHelper(emitter, 'Input data must be Object');
+            return;
+        }
+
+        await writeCSV({ msg, cfg, emitter, token });
+        emitter.emit('end');
         return;
+    } catch (err) {
+        if (emitter) {
+            emitter.emit('error', err);
+            emitter.logger.error(`Error during processing: ${err}`);
+        } else {
+            this.emit('error', err);
+            this.logger.error(`Error during processing: ${err}`);
+        }
     }
-
-    await writeCSV({ msg, cfg, emitter, token });
-    emitter.emit('end');
-    return;
 }
 
 module.exports.process = writeStream;
